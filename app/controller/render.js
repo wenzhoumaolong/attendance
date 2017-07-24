87bc32840b2ef40dbef98559d1cb0160961cf1ea
Server <527030163@qq.com>
2017-07-24 16:03:02 +0800

    server edit

--- app/controller/render.js
+++ app/controller/render.js
@@ -1,27 +1,28 @@
-module.exports = app => {
+﻿module.exports = app => {
   return class RenderController extends app.Controller {
     * index() {
-      const assets = require('../../dist/assets.json');
+      // const assets = require('../../dist/assets.json');
 
-      const getFinalAssets = (obj) => {
-        obj = obj || {};
-        const scripts = [];
-        const styles = [];
+      // const getFinalAssets = (obj) => {
+      //   obj = obj || {};
+      //   const scripts = [];
+      //   const styles = [];
 
-        Object.keys(obj).sort((a, b) => {
-          return b === 'manifest' ? 1: 0; // 保证manifest永远在最前面, manifest文件是webpack公共部分
-        }).forEach(key => {
-          const asset = obj[key];
-          if (asset) {
-            asset.js && scripts.push(asset.js);
-            asset.css && styles.push(asset.css);
-          }
-        });
+      //   Object.keys(obj).sort((a, b) => {
+      //     return b === 'manifest' ? 1: 0; // 保证manifest永远在最前面, manifest文件是webpack公共部分
+      //   }).forEach(key => {
+      //     const asset = obj[key];
+      //     if (asset) {
+      //       asset.js && scripts.push(asset.js);
+      //       asset.css && styles.push(asset.css);
+      //     }
+      //   });
 
-        return { scripts, styles };
-      };
-
-      yield this.ctx.render('layout.pug', getFinalAssets(assets))
+      //   return { scripts, styles };
+      // };
+      yield this.ctx.render('layout.pug', {
+        scripts: ['/vendor.js', '/app.js']
+      });
     }
   }
 }
--- config/config.default.js
+++ config/config.default.js
@@ -1,4 +1,4 @@
-const path = require('path');
+﻿const path = require('path');
 
 module.exports = appInfo => {
   return {
@@ -50,7 +50,7 @@ module.exports = appInfo => {
         // 用户名
         user: 'root',
         // 密码
-        password: 'abc123_',
+        password: 'root',
         // 数据库名
         database: 'attendance',
       },
